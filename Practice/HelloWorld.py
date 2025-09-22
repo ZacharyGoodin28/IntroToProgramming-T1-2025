86c2d989a42ac988bcce8e1f691a9b7acaa9b24d
Goodin Zachary <828008@stma.k12.mn.us>
2025-09-22 13:59:52 -0500

    Initial Commit

---
+++ Practice/HelloWorld.py
@@ -0,0 +1,14 @@
+# Data Types
+# Boolean - true/false
+# Character - a single keyboard symbol
+
+print("Hello World!")
+print("Hello Zachary!")
+
+
+print("Top 5 Movies")
+print("1.Step Brothers")
+print("2.Wolverine")
+print("3.The Amazing Spiderman")
+print("4.The Dark Knight")
+print("5.Iron Man 3")
