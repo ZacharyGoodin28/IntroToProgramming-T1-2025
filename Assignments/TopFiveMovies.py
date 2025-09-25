26cd5ca0c2ed6cab21172422f00880f991052203
Goodin Zachary <828008@stma.k12.mn.us>
2025-09-25 13:15:13 -0500

    message

--- Assignments/TopFiveMovies.py
+++ Assignments/TopFiveMovies.py
@@ -1,3 +1,8 @@
-
+print ("TopFiveMovies")
+print ("Batman")
+print ("Spider-Man")
+print ("Iron Man 3")
+print ("Alien")
+print ("Predator")
 
 
---
+++ Practice/2.3-praactice.py
@@ -0,0 +1,28 @@
+# String    - "in quotations"
+# Interger  - a whole number ex. 1 4 -10 ect.
+# Float     - a decimal umber ex. 3.14159265359
+# Char      - a single glyph ex.  'c'
+# Bool      - True or False
+
+# How to define a variable!!
+# <name> = <value>
+
+x = 10
+y = 50
+potato = 7
+
+print(x * y * potato)
+
+
+lowercase = True
+UPPERCASE = True
+camelCase = True   #ALL Lowercase, no spaces, capital for new words
+UppercCamelCase = True
+lowerCamelCase = True
+snake_case = True
+SCREAMING_SNAKE_CASE = False
+
+# Other general rules to naming things
+# 1. Concise
+#  -
+
