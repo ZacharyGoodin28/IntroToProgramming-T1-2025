6215f0f0813f1c8cc0a30eaf5672980cf7dd760e
Goodin Zachary <828008@stma.k12.mn.us>
2025-10-06 13:48:35 -0500

    unit2test

---
+++ Assignments/unit2test.py
@@ -0,0 +1,11 @@
+def add_three(a, b, c):
+    print(int(a) + (int(b) + int(c)))
+
+
+add_three(input("Enter the first number you want to add\n>"), input("Enter the second number you want to add\n>"), input("Enter the third number you want to add\n>"))
+
+def data_three(1, 2, 3):
+    print(int(1) + (int(2) + int(3)))
+
+
+data_three(input("Enter the first word you want to add\n>"), input("Enter the first integer you want to add\n>"), input("Enter the first float you want to add\n>"))
